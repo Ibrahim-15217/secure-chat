a4b9e536464492a9ad21f96a3b7c8d011b3d9aae
Ibrahim Musa Mahmud <ibrahimmusamahmud01@gmail.com>
2026-09-12 16:07:31 +0100

    revert: remove temporary 500 detail leak (diagnosis complete)

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -23,7 +23,7 @@ function handleAuthError(res, err) {
   }
   // eslint-disable-next-line no-console
   console.error(err);
-  return res.status(500).json({ error: 'Internal server error', detail: err.stack });
+  return res.status(500).json({ error: 'Internal server error' });
 }
 
 router.post('/register', async (req, res) => {
